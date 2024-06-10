fe2752af4dd4ea5870cbf1af940dadcfb52723f6
veggiecan0419 <137440114+veggiecan0419@users.noreply.github.com>
2024-06-10 07:49:23 +0300

    Fix

--- src/lib/themes/accent/palestine.js
+++ src/lib/themes/accent/palestine.js
@@ -3,7 +3,6 @@ import icon from './palestine-flag.svg';
 import watermelonStrip from './palestine-bg-strip.svg';
 
 const guiColors = {
-    /*
     'motion-primary': '#ff4c4c',
     'motion-primary-transparent': '#ff4c4ce6',
     'motion-tertiary': '#cc3333',
@@ -17,24 +16,21 @@ const guiColors = {
     'extensions-tertiary': 'hsla(10, 85%, 40%, 1)',
     'extensions-transparent': 'hsla(10, 85%, 65%, 0.35)',
     'extensions-light': 'hsla(10, 57%, 85%, 1)',
-    */
 
     'menu-bar-background-image': `url('${watermelonStrip}')`,
     'menu-bar-background-repeat': 'repeat-x',
     'menu-bar-background-size': "contain",
 
-    // 'drop-highlight': '#ff8c8c',
+    'drop-highlight': '#ff8c8c',
 };
 
-/*
 const blockColors = {
     checkboxActiveBackground: '#ff4c4c',
     checkboxActiveBorder: '#cc3333'
 };
-*/
 
 export {
     guiColors,
-    // blockColors,
+    blockColors,
     icon
 };
